6b67c0a27db962ee28f3638cc51bdedba5a2de19
Hasan Basri <hbasria@gmail.com>
2017-12-14 10:09:13 +0300

    dependency updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as fp:
 
 setup(
     name='bitutils',
-    version='0.1.1',
+    version='0.1.2',
     description='Crypto Currency utility library',
     long_description=long_description,
     author='Hasan Basri',
@@ -16,6 +16,7 @@ setup(
     keywords='Crypto Currency Utility',
     url='https://github.com/hbasria/bitutils',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['py_dictutils~=0.1.0'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
