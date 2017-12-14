3b95109039cdb7772250a6f767865b88106eecb7
Hasan Basri <hbasria@gmail.com>
2017-12-14 09:45:07 +0300

    Ticker object added

--- bitutils/objects.py
+++ bitutils/objects.py
@@ -2,8 +2,9 @@ from dictutils import AttrDict
 
 
 class Exchange(AttrDict):
-    def __init__(self, name=None, **kwargs):
+    def __init__(self, code=None, name=None, **kwargs):
         super(Exchange, self).__init__(**kwargs)
+        self.code = code
         self.name = name
 
 
@@ -16,7 +17,7 @@ class Currency(AttrDict):
         self.symbol = symbol
         self.decimals = decimals
         self.type = type
-        self.exchanges = []
+        self.address = None
 
 
 class Market(AttrDict):
@@ -26,4 +27,24 @@ class Market(AttrDict):
         self.name = name
         self.base = base
         self.quote = quote
-        self.exchanges = []
+
+
+class Ticker(AttrDict):
+    def __init__(self, bid=None, ask=None, last=None, low=None, high=None, **kwargs):
+        super(Ticker, self).__init__(**kwargs)
+        self.bid = bid
+        self.ask = ask
+        self.last = last
+        self.low = low
+        self.high = high
+
+# {
+#     "mid": "244.755",
+#     "bid": "244.75",
+#     "ask": "244.76",
+#     "last_price": "244.82",
+#     "low": "244.2",
+#     "high": "248.19",
+#     "volume": "7842.11542563",
+#     "timestamp": "1444253422.348340958"
+# }
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('README.md') as fp:
 
 setup(
     name='bitutils',
-    version='0.1.0',
+    version='0.1.1',
     description='Crypto Currency utility library',
     long_description=long_description,
     author='Hasan Basri',
     author_email='hbasria@gmail.com',
     license='MIT',
     keywords='Crypto Currency Utility',
-    url='https://github.com/hbasria/py-registry',
+    url='https://github.com/hbasria/bitutils',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     classifiers=[
         'Development Status :: 4 - Beta',
